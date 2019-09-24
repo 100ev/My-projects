0c8fe239ff1bda5c6f3b276b62c3a70e961b05f1
Stanimir_Stoev <stanstz@abv.bg>
2019-09-24 16:51:09 +0300

    Add files via upload

---
+++ C#/Most Frequent Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Most_Frequent_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int repet = 0;
+            int mostFrNum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int curNum = arr[i];
+                int count = 0;
+
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (curNum == arr[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > repet)
+                {
+                    mostFrNum = count;
+                    repet = count;
+                }
+            }
+            Console.WriteLine(mostFrNum);
+        }
+    }
+}
