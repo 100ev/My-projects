8291bcbf541700049893e94fbb891b91d6eab38f
Stanimir_Stoev <stanstz@abv.bg>
2019-10-15 16:26:59 +0300

    Add files via upload

---
+++ C#/Greater Of Two Values/Greater Of Two Values(Switch-Case)/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greater_Of_Two_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Checker(input);
+        }
+        static void Checker(string input)
+        {
+            switch (input)
+            {
+                case ("int"):
+                    int num1 = int.Parse(Console.ReadLine());
+                    int num2 = int.Parse(Console.ReadLine());
+                    int max = 0;
+
+                    if (num1 > num2)
+                    {
+                        max = num1;
+                    }
+                    else
+                    {
+                        max = num2;
+                    }
+                    Console.WriteLine(max);
+                    break;
+                case ("char"):
+                    char ch1 = char.Parse(Console.ReadLine());
+                    char ch2 = char.Parse(Console.ReadLine());
+                    char maxChar = (char)0x00;
+
+                    if (ch1 > ch2)
+                    {
+                        maxChar = ch1;
+                    }
+                    else
+                    {
+                        maxChar = ch2;
+                    }
+                    Console.WriteLine(maxChar);
+                    break;
+                case ("string"):
+                    string first = Console.ReadLine();
+                    string second = Console.ReadLine();
+                    string maxString = "";
+
+                    if (first.CompareTo(second) > 0)
+                    {
+                        maxString = first;
+                    }
+                    else
+                    {
+                        maxString = second;
+                    }
+                    Console.WriteLine(maxString);
+                    break;
+            }
+        }
+    }
+}
---
+++ C#/Greater Of Two Values/Greater Of Two Values(lIf-Ese)/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greater_Of_Two_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            if (input == "int")
+            {
+                int first = int.Parse(Console.ReadLine());
+                int second = int.Parse(Console.ReadLine());
+                int max = IntMax(first, second);
+                Console.WriteLine(max);
+            }
+            else if (input == "char")
+            {
+                char first = char.Parse(Console.ReadLine());
+                char second = char.Parse(Console.ReadLine());
+                char max = CharMax(first, second);
+                Console.WriteLine(max);
+            }
+            else if (input == "string")
+            {
+                string first = Console.ReadLine();
+                string second = Console.ReadLine();
+                string max = StringMax(first, second);
+                Console.WriteLine(max);
+            }
+
+        }
+        static int IntMax(int first, int second)
+        {
+            int max = 0;
+
+            if (first > second)
+            {
+                max = first;
+            }
+            else
+            {
+                max = second;
+            }
+            return max;
+        }
+                   
+        static char CharMax(char first, char second)
+        {
+            char max = (char)0x00;
+
+            if (first > second)
+            {
+                max = first;
+            }
+            else
+            {
+                max = second;
+            }
+            return max;
+        }
+        
+        static string StringMax(string first, string second)
+        {
+            string max = "";
+
+            if (first.CompareTo(second) > 0)
+            {
+                max = first;
+            }
+            else
+            {
+                max = second;
+            }
+            return max;
+        }
+    }
+}
