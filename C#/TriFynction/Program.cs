6b627b285f8fbef4cc69470dc38f371fddaa536a
Stanimir_Stoev <st100ev@abv.bg>
2020-10-21 16:32:47 +0300

    Add files via upload

---
+++ C#/TriFynction/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace TriFynction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            var names = Console.ReadLine().Split();
+
+            Func<string, int, bool> isValidWord = (str, num) => str.ToCharArray().Select(ch => (int)ch).Sum() >= num;
+            Func<string[], int, Func<string, int, bool>, string> firtsValidName = (arr, num, func) => arr.FirstOrDefault(str => func(str, num));
+
+            var result = firtsValidName(names, number, isValidWord);
+            Console.WriteLine(result);
+        }
+    }
+}
